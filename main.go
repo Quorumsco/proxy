91d2f3ea42bd0ce50cf00f31d397cb0a67fde18e
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-07-09 16:21:54 +0200

    Answer to all requests

--- main.go
+++ main.go
@@ -8,8 +8,10 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
 	"runtime"
 	"strings"
+	"time"
 
 	"gopkg.in/redis.v3"
 
@@ -74,13 +76,26 @@ func serve(ctx *cli.Context) error {
 		return err == nil || (req.Method == "POST" && req.FormValue("username") != "" && req.FormValue("password") != "") // The form is already parsed.
 	})
 
+	proxy.NonproxyHandler = http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		dump, _ := httputil.DumpRequest(req, true)
+		fmt.Println(string(dump))
+		req.URL.Scheme = req.Header.Get("X-Scheme")
+		req.URL.Host = req.Host
+		// fmt.Println(req.URL.IsAbs())
+		time.Sleep(1 * time.Second)
+		proxy.ServeHTTP(w, req)
+		// http.Error(w, "This is a proxy server. Does not respond to non-proxy requests.", 500)
+	})
+
 	proxy.OnRequest(session).DoFunc(
 		func(req *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+			dump, _ := httputil.DumpRequest(req, true)
+			fmt.Println(string(dump))
 			cookie, err := req.Cookie("SID")
 			if err == nil {
 				session, err := store.Load(cookie.Value)
 				if err != nil {
-					return nil, nil
+					return req, nil
 				}
 				req.Header.Add("Authorization", "Bearer "+session.AccessToken)
 				return req, nil
@@ -102,22 +117,22 @@ func serve(ctx *cli.Context) error {
 				req.ContentLength = int64(v.Len())
 			}
 
-			req.RequestURI = "" // Must be removed for client requests
+			//req.RequestURI = "" // Must be removed for client requests
 			client := &http.Client{}
 			resp, err := client.Do(req)
 			if err != nil {
-				return nil, nil
+				return req, nil
 			}
 			defer resp.Body.Close()
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				return nil, nil
+				return req, nil
 			}
 
 			// TODO: Check http status for errors
 			access := new(components.AccessData)
 			if err := json.Unmarshal(body, access); err != nil {
-				return nil, nil
+				return req, nil
 			}
 			session := &components.Session{
 				ID:           strings.TrimRight(base64.StdEncoding.EncodeToString(uuid.NewRandom()), "="),
@@ -126,14 +141,14 @@ func serve(ctx *cli.Context) error {
 				ExpiresIn:    access.ExpiresIn,
 			}
 			if err := store.Save(session); err != nil {
-				return nil, nil
+				return req, nil
 			}
 
 			// TODO: Give a json response to clients
 			resp = goproxy.NewResponse(req, "text/plain", http.StatusOK, "")
 			cookie = &http.Cookie{Name: "SID", Value: session.ID}
 			resp.Header.Add("Set-Cookie", cookie.String())
-			return nil, resp
+			return req, resp
 		},
 	)
 
